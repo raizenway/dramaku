9782e5afcf535838bb1ba58993ddfb7ba99d0168
Bhisma Chandra <bhisma.chandra.tif422@polban.ac.id>
2024-11-03 19:45:07 +0700

    Review Status

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -11,8 +11,15 @@ class ReviewController extends Controller
 {
     public function index()
     {
-        $reviews = Review::with('user', 'movie')->get();
-
+        $reviews = Review::with('user', 'movie')
+            ->orderByRaw("CASE status 
+                WHEN 'pending' THEN 1 
+                WHEN 'rejected' THEN 2 
+                WHEN 'approved' THEN 3 
+                ELSE 4 END ASC")
+            ->orderBy('created_at', 'asc')
+            ->get();
+    
         return Inertia::render('CMS/CMSReviews', [
             'reviews' => $reviews
         ]);
